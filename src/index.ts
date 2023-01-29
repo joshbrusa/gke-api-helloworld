ca1100d39f64fe0c2b5396df039ee35deae751a4
joshbrusa <joshbrusa@gmail.com>
2023-01-29 13:43:49 -0800

    main

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = 80;
+
+app.use(cors());
 
 app.listen(port);
 
